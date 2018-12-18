91da1a121353966d2eaba82700a896c45c02fcd3
ammsan <45367458+ammsan@users.noreply.github.com>
2018-12-17 20:18:33 -0500

    Create UsersRepositoryImpl.java

---
+++ associate/src/main/java/com/infodashboard/associate/UsersRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.infodashboard.associate;
+
+import com.infodashboard.associate.AssociateRecords;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UsersRepositoryImpl extends UsersRepository<AssociateRecords, Long> {
+  
+}
