82daed90c3984d790ce0c83069590afbaf1833a2
ammsan <45367458+ammsan@users.noreply.github.com>
2018-12-19 20:56:41 -0500

    Update AssociateNotFoundException.java

--- associate/src/main/java/com/infodashboard/associate/AssociateNotFoundException.java
+++ associate/src/main/java/com/infodashboard/associate/AssociateNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class StudentNotFoundException extends RuntimeException {
+public class AssociateNotFoundException extends RuntimeException {
 
 	public AssociateNotFoundException(String exception) {
 		super(exception);
